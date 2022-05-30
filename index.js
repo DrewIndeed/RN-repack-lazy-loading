830d8794da86d84e194f4b2fe9e382a7ecc1ced5
Andrew Le <s3820098@rmit.edu.vn>
2022-05-30 12:06:58 +0700

    install async storage + pod install + npx run ios + yarn start

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { AppRegistry } from 'react-native';
 import { ChunkManager } from '@callstack/repack/client';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import App from './src/App';
+import { AppRegistry } from 'react-native';
+import App from './App';
 import { name as appName } from './app.json';
 
 ChunkManager.configure({
   storage: AsyncStorage,
-  resolveRemoteChunk: async (chunkId) => {
+  resolveRemoteChunk: async chunkId => {
     return {
       url: `http://localhost:5000/${chunkId}`,
     };
