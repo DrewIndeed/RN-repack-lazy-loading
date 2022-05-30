c25aa0f4f75e87f42ab25bb98e7b2bb9220f031a
DrewIndeed <truongan1730@gmail.com>
2022-05-29 21:19:54 -0700

    config for Repack
    todo: pod install on macOS

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { ChunkManager } from '@callstack/repack/client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import App from './src/App';
+import { name as appName } from './app.json';
+
+ChunkManager.configure({
+  storage: AsyncStorage,
+  resolveRemoteChunk: async (chunkId) => {
+    return {
+      url: `http://localhost:5000/${chunkId}`,
+    };
+  },
+});
 
 AppRegistry.registerComponent(appName, () => App);
